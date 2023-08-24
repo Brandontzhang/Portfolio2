3d5b512372095c5c5dcfb72a485d7d8127402658
Brandontzhang <zhangtbrandon2@gmail.com>
2023-08-24 17:23:28 -0400

    add schemas to index

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,3 +1,15 @@
-import about from "./about"
+import about from "./about";
+import contact from "./contact";
+import experience from "./experience";
+import projects from "./projects";
+import skills from "./skills";
+import workExperience from "./workExperience";
 
-export const schemaTypes = [about]
+export const schemaTypes = [
+    about,
+    contact,
+    experience,
+    projects,
+    skills,
+    workExperience
+]
