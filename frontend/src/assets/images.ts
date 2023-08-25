20308c490752842aff99caedb7ed7da98697506b
Brandontzhang <zhangtbrandon2@gmail.com>
2023-08-25 15:40:40 -0400

    Add logo to Navbar

---
+++ frontend/src/assets/images.ts
@@ -0,0 +1,1 @@
+export { default as b_logo } from './b_logo.png';
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+import { b_logo } from '../assets/images';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <img className='aspect-[150px_160px] h-[50px] p-2' src={b_logo}></img>
+    </div>
   )
 }
 
