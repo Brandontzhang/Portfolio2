203beee6c8c9eabc9a73e591567f3835c23b6bf0
Brandontzhang <zhangtbrandon2@gmail.com>
2023-08-26 13:24:21 -0400

    Added base text for header

--- frontend/src/container/Header.tsx
+++ frontend/src/container/Header.tsx
@@ -1,7 +1,33 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import { useRef } from "react";
+import { brandon } from "../assets/images";
+import { cn } from "../utils";
 
-export default Header
+const Header = () => {
+
+  const intro = useRef<HTMLDivElement>(null);
+
+  return (
+    <header className="h-screen pt-16 md:pt-24">
+      <section ref={intro} className={cn(
+        "flex w-fit flex-row items-center justify-end bg-slate-100 drop-shadow-lg rounded-lg m-4 p-4",
+      )}>
+        <span className="m-2 pr-6 text-3xl">👋</span>
+        <div className="flex flex-col justify-start">
+          <p className="text-slate-600">Hello I am</p>
+          <p className="text-5xl">Brandon</p>
+        </div>
+      </section>
+
+      <section>
+        <p>Software Solutions Engineer</p>
+        <p>Web Developer</p>
+      </section>
+
+      <section>
+        <img src={brandon} />
+      </section>
+    </header>
+  );
+};
+
+export default Header;
