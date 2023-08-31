f7c96d4c98dd0555944f5dab282b075d8a598090
brandontzhang <zhangtbrandon2@gmail.com>
2023-08-30 21:11:25 -0400

    Fixed positioning for social media buttons

--- frontend/src/components/SocialMediaButtons.tsx
+++ frontend/src/components/SocialMediaButtons.tsx
@@ -5,15 +5,15 @@ import { MdEmail } from 'react-icons/md';
 
 const SocialMediaButtons = () => {
     return (
-        <section className='fixed bottom-4 text-3xl z-40'>
-            <div className='flex flex-col mx-4'>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer'>
+        <section className='fixed bottom-10 lg:left-10 md:text-3xl z-40'>
+            <div className='flex flex-col mx-4 bg-slate-100 rounded-full border border-slate-300 drop-shadow-lg'>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
                     <MdEmail />
                 </div>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer'>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
                     <BsLinkedin />
                 </div>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer'>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
                     <FaGithub />
                 </div>
             </div>
