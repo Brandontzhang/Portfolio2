3ac6154b4b4071835a8596d6d7bbf5f98ccaa82e
brandontzhang <zhangtbrandon2@gmail.com>
2023-08-30 21:13:14 -0400

    General hook for Sanity queries

---
+++ frontend/src/hooks/useQuerySanity.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { client } from "../client";
+
+export const useQuerySanity = <T>(query : string) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState<T[]>([]);
+    const [error, setError] = useState<any>();
+    
+    const fetchData = async () => {
+        try {
+            let data = await client.fetch(query);
+            setIsLoading(false);
+            setData(data);
+        } catch(err) {
+            setIsLoading(false);
+            setError(err);
+        }
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchData();
+    }, []);
+
+    return { isLoading, data, error };
+}
