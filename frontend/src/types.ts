21b1aa577a6632331cf81c52f981f8eb21bb4d0b
brandontzhang <zhangtbrandon2@gmail.com>
2023-09-01 18:29:23 -0400

    Add types for skill level in frontend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -14,4 +14,22 @@ export type Project = {
 export type Skill = {
     name: string,
     icon: string,
+    skilllevel: SkillLevel
+}
+
+export type SkillLevel = {
+    name: string,
+    order: number,
+}
+
+export type Experience = {
+    year: string,
+    work: Work[],
+}
+
+export type Work = {
+    name: string,
+    company: string,
+    desc: string,
+    imgUrl: string
 }
