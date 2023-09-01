47c7b39a4c291c224574e6b68f9b26d878eb7f65
brandontzhang <zhangtbrandon2@gmail.com>
2023-08-31 20:48:04 -0400

    Added section wrapper for work (indicate current section on nav buttons)

--- frontend/src/container/Work.tsx
+++ frontend/src/container/Work.tsx
@@ -3,6 +3,7 @@ import WorkTypeButton from "../components/WorkSectionComponents/WorkTypeButton";
 import { WorkTypeContextProvider } from "../context/WorkTypeContextProvider";
 import { useQuerySanity } from "../hooks/useQuerySanity";
 import { Project } from "../types";
+import SectionWrap from "../wrapper/SectionWrap";
 
 const Work = () => {
   const WorkTypes = ["Work", "Project", "Web App", "All"];
@@ -12,28 +13,30 @@ const Work = () => {
   );
 
   return (
-    <WorkTypeContextProvider>
-      <section className="flex w-full flex-col items-center justify-center lg:mt-36 overflow-hidden">
-        <h2 className="flex w-full justify-center font-bold sm:text-xl">
-          <span className="text-center text-[1.25rem] md:text-3xl lg:text-5xl">
-            My <span className="text-orange-400">Work Experiences</span> and{" "}
-            <span className="text-orange-400">Projects</span>
-          </span>
-        </h2>
+    <SectionWrap sectionName="work">
+      <WorkTypeContextProvider>
+        <section className="flex w-full flex-col items-center justify-center overflow-hidden lg:mt-36">
+          <h2 className="flex w-full justify-center font-bold sm:text-xl">
+            <span className="text-center text-[1.25rem] md:text-3xl lg:text-5xl">
+              My <span className="text-orange-400">Work Experiences</span> and{" "}
+              <span className="text-orange-400">Projects</span>
+            </span>
+          </h2>
 
-        <section className="flex w-[95dvw] justify-center overflow-x-auto whitespace-nowrap">
-          {WorkTypes.map((workType: string) => (
-            <WorkTypeButton
-              key={workType}
-              className="text-sm md:text-xl"
-              label={workType}
-            />
-          ))}
+          <section className="mt-5 flex w-[95dvw] justify-center overflow-x-auto whitespace-nowrap sm:mt-10">
+            {WorkTypes.map((workType: string) => (
+              <WorkTypeButton
+                key={workType}
+                className="text-sm md:text-xl"
+                label={workType}
+              />
+            ))}
+          </section>
+
+          <ProjectCardCarousel projects={projects} />
         </section>
-
-        <ProjectCardCarousel projects={projects} />
-      </section>
-    </WorkTypeContextProvider>
+      </WorkTypeContextProvider>
+    </SectionWrap>
   );
 };
 
