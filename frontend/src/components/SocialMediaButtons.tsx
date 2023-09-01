4bd13aed3141d1cab8d3f152ce873a3142be7f90
brandontzhang <zhangtbrandon2@gmail.com>
2023-08-31 20:50:00 -0400

    Add size transform for hovering over social media buttons

--- frontend/src/components/SocialMediaButtons.tsx
+++ frontend/src/components/SocialMediaButtons.tsx
@@ -7,13 +7,13 @@ const SocialMediaButtons = () => {
     return (
         <section className='fixed bottom-10 lg:left-10 md:text-3xl z-40'>
             <div className='flex flex-col mx-4 bg-slate-100 rounded-full border border-slate-300 drop-shadow-lg'>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer hover:scale-125 transition-transform duration-200 ease-in-out'>
                     <MdEmail />
                 </div>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer hover:scale-125 transition-transform duration-200 ease-in-out'>
                     <BsLinkedin />
                 </div>
-                <div className='m-4 hover:text-amber-600 hover:cursor-pointer '>
+                <div className='m-4 hover:text-amber-600 hover:cursor-pointer hover:scale-125 transition-transform duration-200 ease-in-out'>
                     <FaGithub />
                 </div>
             </div>
