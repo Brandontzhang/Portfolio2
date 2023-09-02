7bda9cfe61264f94f48db0d4aa88355ddbba1833
brandontzhang <zhangtbrandon2@gmail.com>
2023-09-02 08:47:34 -0400

    Date parsing utility function

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -4,3 +4,9 @@ import { clsx, ClassValue } from 'clsx';
 export const cn = (...inputs : ClassValue[]) => {
     return twMerge(clsx(inputs));
 }
+
+export const getMonthYear = (date : Date) => {
+    const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+    return `${months[date.getMonth()]} ${date.getFullYear()}`;
+}
