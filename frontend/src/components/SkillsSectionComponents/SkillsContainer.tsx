3335588ffcb3ae3fabe052607d936de08ceaf270
brandontzhang <zhangtbrandon2@gmail.com>
2023-09-02 07:03:56 -0400

    Cleanup

--- frontend/src/components/SkillsSectionComponents/SkillsContainer.tsx
+++ frontend/src/components/SkillsSectionComponents/SkillsContainer.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useQuerySanity } from "../../hooks/useQuerySanity";
 import { Skill, SkillLevel } from "../../types";
 import { cn } from "../../utils";
