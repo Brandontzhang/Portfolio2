bd76963feeb08d1fcd130912f5d65679ef067a99
brandontzhang <zhangtbrandon2@gmail.com>
2023-09-06 20:51:13 -0400

    Added extra details hover box

--- frontend/src/components/ExperienceSectionComponents/WorkDetails.tsx
+++ frontend/src/components/ExperienceSectionComponents/WorkDetails.tsx
@@ -1,5 +1,6 @@
 import { Work } from "../../types";
 import { cn, getMonthYear } from "../../utils";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 type WorkDetailsPropsType = {
     work: Work;
@@ -11,11 +12,19 @@ const WorkDetails = ({ work, className }: WorkDetailsPropsType) => {
         <div
             className={cn(
                 className,
-                "mb-10 md:text-2xl italic text-slate-500 text-center sm:text-start",
+                "mb-10 w-full text-center italic text-slate-500 sm:text-start md:text-2xl",
             )}
         >
-            <h3 className="text-2xl md:text-3xl font-bold not-italic text-slate-800">
-                {work.company}
+            <h3 className="group relative flex flex-row justify-center text-2xl font-bold not-italic text-slate-800 sm:justify-start md:text-3xl">
+                <span className="flex flex-row">
+                    {work.company}
+                    <span className="hover:text-orange-400 mx-2">
+                        <AiOutlineInfoCircle />
+                    </span>
+                </span>
+                <div className="absolute top-8 z-50 hidden w-full rounded-xl bg-white p-4 font-normal drop-shadow-lg group-hover:block">
+                    {work.desc}
+                </div>
             </h3>
             <p>{work.name}</p>
             {work.current ? (
